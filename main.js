04cf654a6e1382008d61f7c6115e3a7a4fbc01df
omar123456700 <73769715+omar123456700@users.noreply.github.com>
2021-08-26 15:59:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+
+function setUp(){
+canvas=createCanvas(500,500);
+canvas.position(500,300);
+}
+
+
+function draw(){
+
+}
+
+
+function takeSnapshot(){
+save('your!_LOL_!picture.png');
+}
